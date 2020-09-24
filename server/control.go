3059dd1e00f197a94b91444104c820c74b04d7a7
ttlv <gogophersteven@gmail.com>
2020-09-24 23:27:11 +0800

    feat/server controller: use put replace when frpc lose connection with frps

--- server/control.go
+++ server/control.go
@@ -392,7 +392,7 @@ func (ctl *Control) stoper() {
 	// frpc断开与frps的连接时需要设置hook,通知frp adapter服务将节点设置为离线状态
 	v := url.Values{}
 	v.Add("status", consts.Offline)
-	v.Add("node_maintenance_name", fmt.Sprintf("nodemaintenances-%v", ctl.loginMsg.UniqueID))
+	v.Add("unique_id", ctl.loginMsg.UniqueID)
 	result, err := ttlv_utils.Put(ctl.serverCfg.FrpAdapterServerAddress+"/frp_update", nil, v, nil)
 	if err != nil {
 		xl.Info("update frpc info into k8s failed,err is %v", err)
